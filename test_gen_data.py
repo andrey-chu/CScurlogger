e29b192969635480c91a4ba4f230c50b0940e053
Andrey Chuhutin <end.product@gmail.com>
2019-07-17 00:20:10 +0200

    main testfile updated

--- test_gen_data.py
+++ test_gen_data.py
@@ -7,13 +7,20 @@ Here we check how the data can be generated
 """
 
 import auxgenfuns
+import dicsfuns
 import time
 import gen_data
 import math
-funclist = [auxgenfuns.aux_gen_sin_day,auxgenfuns.aux_gen_sin_year,auxgenfuns.aux_gen_ladder_day]
+funclist = [auxgenfuns.aux_gen_sin_workday,auxgenfuns.aux_gen_sin_year,auxgenfuns.aux_gen_ladder_day]
 freq = 1
-synchr = True
+synchr = False
 stoptime = math.floor(time.time()+60*60)         #1 hour
-artificial_data_list = gen_data.gen_artificial_data(funclist, freq, synchr, stoptime)
+artificial_data_dict= gen_data.gen_artificial_data(funclist, freq, synchr, stoptime)
+starttime = math.floor(time.time()+60*30)         #1 hour
+stoptime = math.floor(time.time()+60*90)         #1 hour
+artificial_data_dict_later= gen_data.gen_artificial_data(funclist, freq, synchr, stoptime, starttime)
+data_dict_union = dicsfuns.union_dics(artificial_data_dict, artificial_data_dict_later)
+data_dict_low = dicsfuns.lower_than_key(data_dict_union, starttime-1000)
+data_dict_high = dicsfuns.higher_or_eq_than_key(data_dict_union, starttime-1000)
 
 #print(artificial_data_list)
